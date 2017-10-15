127a1861c53bbab12cd662435a8439e3e64d598e
Luis Zavala <cionagi@hotmail.cl>
2017-10-15 10:01:05 -0300

    Finish Web

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,309 @@
-console.log('\'Allo \'Allo!');
+const data_felina = [
+  {
+    titulo: 'Una Mujer Fantástica',
+    director: 'Sebastián Lelio',
+    duracion: '104 min',
+    description: 'Marina (Daniela Vega) una joven camarera aspirante a cantante y Orlando (Francisco Reyes), veinte años mayor, planean un futuro juntos. Tras una noche de fiesta, Marina lo lleva a urgencias, pero él muere al llegar al hospital. Ella debe entonces enfrentar las sospechas por su muerte. Su condición de mujer transexual supone para la familia de Orlando una completa aberración. Ella tendrá que luchar para convertirse en lo que es: una mujer fuerte, pasional... fantástica. fuente: cinechile.cl',
+    ano: '2017',
+    pais: 'Chile',
+    horarios: [{
+      lugar: 'Teatro Municipal. Independencia 520. Linares',
+      hora: '19:30hrs',
+    }],
+    fecha_larga: '19 de octubre 2017',
+    fecha_corta: '19/10/2017',
+    link: 'https://www.youtube.com/embed/NLRRf6y-7X8',
+    tipo: 'Película'
+  },
+  {
+    titulo: 'El Cristo Ciego',
+    director: 'Christopher Murray',
+    duracion: '85 min',
+    description: 'Rafael es un mecánico de 30 años que dice haber vivido una revelación divina en el desierto. Pero lejos de creerle, sus vecinos lo tratan como al loco del pueblo. Una tarde se entera de que un amigo de la infancia ha sufrido un accidente en un pueblo lejano. Michael decide abandonarlo todo para hacer un peregrinaje a pie, descalzo, y sanarlo con un milagro. Su marcha comienza a llamar la atención de la gente explotada por las empresas mineras y adicta a las drogas, que lo ven como un Cristo capaz de aliviar la cruda realidad del desierto chileno. Fuente es jirafa.cl',
+    ano: '2017',
+    pais: 'Chile',
+    horarios: [{
+      lugar: 'Teatro Municipal. Independencia 520. Linares',
+      hora: '19:30hrs',
+    }],
+    fecha_larga: '20 de octubre 2017',
+    fecha_corta: '20/10/2017',
+    link: 'https://www.youtube.com/embed/3Qw4yq2q6pk',
+    tipo: 'Película'
+  },
+  {
+    titulo: 'Mala Junta',
+    director: 'Claudia Huaiquimilla',
+    duracion: '89 min',
+    description: 'El Taller "La mujer en el cine y la publicidad: presencias y evasiones" tiene como objetivo analizar y deconstruir las representaciones de género presentes en la publicidad y el cine. A partir de herramientas como la perspectiva de género se estudiarán modelos femeninos y masculinos que pueden encontrarse en los anuncios publicitarios y el cine, poniendo especial atención en conceptos como la violencia simbólica y en modelos como las amas de casa, las mujeres objeto o las madres. De tal manera este taller se presenta como una propuesta dinámica, necesaria y divertida para desentrañar los mecanismos de la construcción de las representaciones de género en el cine y la publicidad. fuente: cinechile.cl',
+    ano: '2017',
+    pais: 'Chile',
+    horarios: [{
+      lugar: 'Casa Grande. Maipú #1059. Linares',
+      hora: '20:00hrs',
+    }],
+    fecha_larga: '27 de octubre 2017',
+    fecha_corta: '27/10/2017',
+    link: 'https://www.youtube.com/embed/AdW5k9Av9II',
+    tipo: 'Película'
+  },
+  {
+    titulo: 'Cabros de Mierda',
+    director: 'Gonzalo Justiniano',
+    duracion: '118 min',
+    description: 'La Victoria, 1983. Gladys (32), conocida como “La Francesita”, es una joven mujer chilena, de carácter, atractiva y valiente, que vive el día a día en un barrio marginal de Santiago, en el Chile de Pinochet. Gladys vive con su madre (Gladys) y su pequeña hija (Gladys). Las tres Gladys reciben en su casa a un ingenuo joven misionero norteamericano llamado Samuel Thompson (23), que viene a predicar la palabra de Dios y las bondades del progreso para este país del tercer mundo. Con su cámara, Samuel registrará cómo la población lucha por sobrevivir en una situación de normal anormalidad, entre ollas comunes, niños sin padres y las primeras grandes protestas. Fuente: Filmaffinity.com',
+    ano: '2017',
+    pais: 'Chile',
+    horarios: [{
+      lugar: 'Teatro Municipal, Independencia #520. Linares',
+      hora: '19:30hrs',
+    }],
+    fecha_larga: '23 de octubre 2017',
+    fecha_corta: '23/10/2017',
+    link: 'https://www.youtube.com/embed/Mb9810YHQJo&t=2s',
+    tipo: 'Película'
+  },
+  {
+    titulo: 'Las Malas Intenciones',
+    director: 'Rosario García Montero',
+    duracion: '110 min',
+    description: 'Relata un periodo en la vida de Cayetana, una niña de ocho años que crece en el Perú de comienzos de los 80, cuando la violencia terrorista comenzaba a agitar al país. La historia transcurre según el punto de vista de esta niña inteligente, pero con una personalidad algo oscura y distorsionada. Hija de padres separados, Cayetana está sola y bajo el cuidado de sus empleados. Tras regresar de un largo viaje, su madre, Inés, le da una noticia inesperada: está embarazada. El frágil mundo de Cayetana se desmorona. La niña se encierra en su cuarto y declara de manera solemne que el día del nacimiento de su hermano será el día de su propia muerte. Sólo su imaginación y la irrupción de los héroes nacionales de sus textos escolares – Olaya, Grau, Bolognesi – podrán salvarla de un entorno familiar cada vez más ajeno en un país a punto de derrumbarse. Fuente: Lasmalasintenciones.com',
+    ano: '2011',
+    pais: 'Chile',
+    horarios: [{
+      lugar: 'Centro de Extensión UTAL. 2 norte #685, Talca',
+      hora: '19:30hrs',
+    }],
+    fecha_larga: '26 de octubre 2017',
+    fecha_corta: '26/10/2017',
+    link: 'https://www.youtube.com/embed/b7SHZF3_P8E',
+    tipo: 'Película'
+  },
+  {
+    titulo: 'El Viento Sabe Que Vuelvo a Casa',
+    director: 'José Luis Torres Leiva',
+    duracion: '102 min',
+    description: 'El documentalista Chileno Ignacio Agüero prepara su primer largometraje de ficción basado en un antiguo proyecto documental que nunca llevó a cabo. A comienzos de los años 80, en la Isla Meulín, región de Chiloé, una joven pareja de novios desaparece en los bosques de la zona sin dejar rastro alguno. Todo un mito se creó en torno a esta misteriosa historia de amor trágico. Ignacio Agüero viajará al lugar de los hechos en busca de locaciones y actores no profesionales para finalmente descubrir poco a poco el desarrollo de su película. Fuente: Filmaffinity.com',
+    ano: '2016',
+    pais: 'Chile',
+    horarios: [{
+      lugar: 'Salón Balmaceda de Gobernación Provincial. Manuel Rodríguez #580. Linares',
+      hora: '19:30hrs',
+    }],
+    fecha_larga: '25 de octubre 2017',
+    fecha_corta: '25/10/2017',
+    link: 'https://www.youtube.com/embed/n7bHWty_C-M',
+    tipo: 'Película'
+  },
+  {
+    titulo: 'Río Abajo',
+    director: 'Miguel Frank',
+    duracion: '85 min',
+    description: 'Obligada por su padre, Rosario, una hermosa y sencilla muchacha campesina, viaja en bote por el Maule desde Pichamay a Constitución, para ser empleada en una casa de dudosa reputación. Mientras Doña Justa, dueña del lupanar, intenta enseñarle las artimañas del amor tarifado, la muchacha se enamora de Juan, un rudo y atractivo pescador. Aparece Don Ramoncito, un peluquero que seduce a la inocente joven con las tentaciones de la ciudad. Charito finalmente sucumbe y abandona la caleta para viajar “río abajo” con el estilista. Fuente: Cinechile.cl',
+    ano: '1949',
+    pais: 'Chile',
+    horarios: [{
+      lugar: 'RukaLauken Café & Grill, Ohiggins #228, Colbún.',
+      hora: '20:00hrs',
+    }],
+    fecha_larga: '26 de octubre 2017',
+    fecha_corta: '26/10/2017',
+    link: 'https://www.youtube.com/embed/n7bHWty_C-M',
+    tipo: 'Película'
+  },
+  {
+    titulo: 'Largo Viaje',
+    director: 'Patricio Kaulen',
+    duracion: '83 min',
+    description: 'La muerte de su hermano recién nacido hará que un niño de los barrios pobres de Santiago recorra la ciudad con las alitas perdidas de cartón que el bebé muerte pierde tras el velatorio, intentando devolversélas con la ilusión de que así pueda su hermanito volar al cielo. Antecedente directo del Nuevo Cine Chileno, es considerada una de las películas más importantes de la cinematografía local. Influída claramente por el neorrealismo italiano, el filme toma como base de su historia la creencia popular del velatorio del angelito, el cual provoca el divagar del niño por Santiago, un Santiago caótico y lleno de diferencias sociales. Fuente: Cinechile.cl',
+    ano: '1949',
+    pais: 'Chile',
+    horarios: [{
+      lugar: 'CFT San Agustín, Colo - Colo #840. Linares',
+      hora: '20:00hrs',
+    }],
+    fecha_larga: '26 de octubre 2017',
+    fecha_corta: '26/10/2017',
+    link: 'https://www.youtube.com/embed/tyOwNEUKvM0',
+    tipo: 'Película'
+  },
+  {
+    titulo: 'Cantar Con Sentido, una biografía de Violeta Parra',
+    director: 'Leonardo Beltrán',
+    duracion: '22 min',
+    description: 'Del campo profundo y carente, a la Europa antigua y distante, luchando siempre adelante, incluso en su despedida galopante, Violeta Parra exhibe sus artes, con sus desventuras mediante. Repasando algunos hitos cruciales que la construyen como persona y artista, esta biografía animada en stop motion nos convida un vistazo a la historia de la mujer que rescató el folclor de un país que olvida. Fuente: Cinechile.cl',
+    ano: '2016',
+    pais: 'Chile',
+    horarios: [{
+      lugar: 'Museo de Arte & Artesanía de Linares. Valentin Letelier 572',
+      hora: '15:00hrs',
+    }, {
+      lugar: 'CFT San Agustín,  Colo - Colo 840. Linares',
+      hora: '17:30hrs',
+    }],
+    fecha_larga: '20 de octubre 2017',
+    fecha_corta: '20/10/2017',
+    link: 'https://www.youtube.com/embed/qd3tuehCw24',
+    tipo: 'Película'
+  },
+  {
+    titulo: 'Conversatorio: La mujer en el cine y la publicidad: presencias y evasiones',
+    director: null,
+    duracion: null,
+    description: 'Cuando Tano (16) vuelve a cometer un delito es enviado a vivir con su padre al campo, donde se hace amigo de un tímido joven mapuche llamado Cheo (15). Un conflicto político en el sector y las malas relaciones con sus padres, los desafían a enfrentar juntos los prejuicios con que cargan en su ya complicada adolescencia. Relatora: Alejandra Pinto, crítica de cine en Cinetoscopio.cl, Películas D y El Agente Cine ',
+    ano: null,
+    pais: null,
+    horarios: [{
+      lugar: 'Salón Balmaceda de Gobernación Provincial. Manuel Rodríguez #580. Linares',
+      hora: '15:30hrs',
+    }],
+    fecha_larga: '23 de octubre 2017',
+    fecha_corta: '23/10/2017',
+    link: null,
+    tipo: 'Taller'
+  },
+  {
+    titulo: 'Taller para jóvenes «Recursos informáticos para medios de comunicación',
+    director: null,
+    duracion: null,
+    description: 'Este taller se propone abordar las principales transformaciones en el campo de la comunicación y la cultura ocurridas en el último siglo, desde el análisis y la reflexión teórica, en primer lugar, y desde la puesta en práctica de esos conocimientos, a través de un proceso de enseñanza-aprendizaje en torno a la creación de medios de comunicación y otras posibilidades que ofrece hoy internet. Inscripciones gratuitas en felina.cine@gmail.com ',
+    ano: null,
+    pais: null,
+    horarios: [{
+      lugar: 'Liceo María Auxiliadora. O’Higgins #615. Linares',
+      hora: '15:20hrs',
+    }],
+    fecha_larga: '26 de octubre 2017',
+    fecha_corta: '26/10/2017',
+    link: null,
+    tipo: 'Taller'
+  },
+]
+let programa = (function () {
+  function draw() {
+    let html = ''
+    for (let i = 0; i < data_felina.length; i++) {
+      let j
+      if (i === 0 || i === 6) {
+        html += '<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" >'
+      }
+      let data_class = data_felina[i].tipo === 'Taller' ? 'programa-color-taller' : ''
+      html += '<div class="row programa-info-movie ' + data_class + ' ">'
+      html += '<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 programa-horario">'
+      for (j = 0; j < data_felina[i].horarios.length; j++) {
+        html += data_felina[i].fecha_corta + ' ' + data_felina[i].horarios[j].hora
+        html += '<br>'
+      }
+
+      html += '</div>'
+      html += '<div class="col-lg-9 col-md-9 col-sm-9 col-xs-12">'
+      html += '<p class="programa-title">'
+      html += data_felina[i].titulo
+      html += '</p>'
+      if (data_felina[i].tipo !== 'Taller') {
+        html += '<p class="programa-subtitle">'
+        html += 'Director:' + data_felina[i].director + '(' + data_felina[i].pais + ',' + data_felina[i].duracion + ',' + data_felina[i].ano
+        html += '</p>'
+      }
+      for (j = 0; j < data_felina[i].horarios.length; j++) {
+        html += '<p class="programa-direction">'
+        html += data_felina[i].horarios[j].lugar
+        html += '</p>'
+      }
+      html += '</div>'
+      html += '</div>'
+      if (i === 5 || i === data_felina.length) {
+        html += '</div>'
+      }
+    }
+
+    $('#programa-print').append(html)
+  }
+
+  function drawResena() {
+    let html = ''
+    for (let i = 0; i < data_felina.length; i++) {
+      if (data_felina[i].tipo !== 'Taller') {
+
+        html += '<div class="section">'
+        html += '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'
+        html += '<div class="embed-responsive embed-responsive-4by3">'
+        html += '<iframe class="embed-responsive-item" src="' + data_felina[i].link + '"></iframe>'
+        html += '</div>'
+        html += '</div>'
+
+        html += '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'
+        html += '<div class="row">'
+        html += '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'
+        html += '<p class="resena-title">' + data_felina[i].titulo + '</p>'
+        html += '<p class="resena-subtitle">Director:' + data_felina[i].director + '(' + data_felina[i].pais + ',' + data_felina[i].duracion + ',' + data_felina[i].ano + '</p>'
+        html += '<p class="resena-text"> ' + data_felina[i].description + ' </p>'
+        html += '</div>'
+        html += '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'
+        html += '<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">'
+        html += '<img src="images/resena-triangulo.png">'
+        html += '</div>'
+        html += '<div class="col-lg-11 col-md-11 col-sm-11 col-xs-10 resena-place">'
+        for (var j = 0; j < data_felina[i].horarios.length; j++) {
+          html += '<p class="resena-">' + data_felina[i].horarios[j].lugar + ' ' + data_felina[i].horarios[j].hora + '</p> '
+        }
+        html += '<p class="resena-">' + data_felina[i].fecha_larga + '</p>'
+
+        html += '</div>'
+        html += '</div>'
+        html += '</div>'
+        html += '</div>'
+        html += '</div>'
+      }
+    }
+    $('#fullpage').append(html).fullpage({
+      responsiveWidth: 768,
+      responsiveHeight: 600
+    })
+  }
+
+  function drawTalleres() {
+    let html = ''
+    for (let i = 0; i < data_felina.length; i++) {
+      if (data_felina[i].tipo === 'Taller') {
+        html += '<div class="section">'
+        html += '<div class="row">'
+        html += '<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-xs-12 taller-titulo">'
+        html += data_felina[i].titulo
+        html += '</div>'
+        html += '<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-xs-12 taller-descripcion">'
+        html += data_felina[i].description
+        html += '</div>'
+        for (var j = 0; j < data_felina[i].horarios.length; j++) {
+          html += '<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-xs-12 talle-direcction">'
+          html += data_felina[i].horarios[j].lugar
+          html += '</div>'
+          html += '<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-xs-12 taller-horario">'
+          html += data_felina[i].horarios[j].hora
+          html += '</div>'
+        }
+        html += '<div class="col-lg-8 col-lg-offset-2 col-md-8 col-md-offset-2 col-sm-12 col-xs-12 taller-extra">'
+        html += 'Inscripciones gratuitas en felina.cine@gmail.com'
+        html += '</div>'
+        html += '</div>'
+        html += '</div>'
+      }
+    }
+    $('#fullpage-talleres').append(html).fullpage({
+      responsiveWidth: 768,
+      responsiveHeight: 600
+    })
+  }
+
+  return {
+    draw,
+    drawResena,
+    drawTalleres
+  }
+})()
+$(document).ready(function () {
+  programa.draw()
+  programa.drawResena()
+  programa.drawTalleres()
+})
+
